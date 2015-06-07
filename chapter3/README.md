7c04715aa3c569827454116f0eae55b9054299ac
大橋 勇希 <y-ohhashi@y-ohhashi-mac2.local>
2015-06-07 15:07:40 +0900

    資料アップ

---
+++ chapter3/README.md
@@ -0,0 +1,27 @@
+# 誤解されない名前
+
+## 3.1 filter
+
+引数を受け取ってるからややこしい。  
+
+```java
+retVal = obj.filterToPcMailAddress()
+```
+
+`select` 又は `exclude` の方が分かりやすい。  
+
+## 3.2 Clip
+
+Clip = 切る  
+最初を切るのか最後を切るのか？  
+
+## list::size
+
+プロパティで持っているサイズを返していると思われがち  
+
+
+## おまけ
+
+`fucking_code1.php` と `fucking_code2.php` のコードと遭遇した際、はむ は残念な気持ちになりました。  
+なぜでしょうか？  
+
---
+++ chapter3/fucking_code2.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($firstEnqueteCampaign != 0 || $womanUserType != 0 || $isInviteTargetUser === true) {
+    // なんかする
+    if ($firstEnqueteCampaign === 1 || $womanUserType === 1) {
+        // なんかする
+    }
+}
+
+if ($firstEnqueteCampaign != 1 || $womanUserType != 1 || $isInviteTargetUser === true) {
+    // なんかする
+    if ($firstEnqueteCampaign === 2 || $womanUserType === 2) {
+        // なんかする
+    }
+}
